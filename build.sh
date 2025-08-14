f49fb4df2aba7d604a0393e65576fdf193cb16b1
34d64 <sadrazarafshan@gmail.com>
2025-08-15 00:55:43 +0330

    Add auto-create superuser script to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 set -o errexit
+
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Auto-create superuser if it doesn't exist
+python - <<END
+import os
+import django
+django.setup()
+from django.contrib.auth.models import User
+
+username = "admin"
+password = "admin"
+email = "admin@example.com"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
+END
