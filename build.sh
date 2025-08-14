8306d2e0196363b7f5ab34e67a0d8a94cdf031ee
34d64 <sadrazarafshan@gmail.com>
2025-08-15 01:01:53 +0330

    Update build.sh with superuser creation

--- build.sh
+++ build.sh
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Make sure DJANGO_SETTINGS_MODULE is set
-export DJANGO_SETTINGS_MODULE=project.settings
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files
 python manage.py collectstatic --no-input
-
-# Apply migrations
 python manage.py migrate
 
-# Create superuser (only if not exists)
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
-if not User.objects.filter(username='admin').exists(): \
-    User.objects.create_superuser('admin','admin@example.com','admin')" | python manage.py shell
-
+# Create superuser if it doesn't exist
+python - <<END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+END
